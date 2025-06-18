02cacefca7d71198187dd26beb55fb6a4ad4de5f
Domotorcr7 <wwslonik228@gmail.com>
2025-06-18 16:48:29 +0300

    Update Work.cpp

--- Work.cpp
+++ Work.cpp
@@ -3,89 +3,149 @@
 int Work(string filename)
 {
     int choice = 0;
-    cout << "Âûáåðèòå îïåðàöèþ:" << endl
-        << "1. Ïðîñìîòð òàáëèöû" << endl
-        << "2. Äîáàâèòü ñòóäåíòà" << endl
-        << "3. Óäàëèòü ñòóäåíòà" << endl
-        << "4. Ïîèñê ñòóäåíòà" << endl
-        << "5. Ýêñïîðò äàííûõ" << endl
-        << "6. Èìïîðò äàííûõ" << endl
-        << "7. Âûõîä" << endl
-        << "Âíèìàíèå, ðàáîòàéòå ñ àíãëèéñêèì ÿçûêîì!!!" << endl;
-    cin >> choice;
+cout << "Выберите функцию:" << endl
+    << "1. Просмотр списка студентов" << endl
+    << "2. Добавить студента" << endl
+    << "3. Удалить студента   " << endl
+    << "4. Поиск студента " << endl
+    << "5. Экспорт данных " << endl
+    << "6. Импорт данных " << endl
+    << "7. Выход" << endl
+    << "Внимание, работайте с английским языком!!!" << endl;
+cin >> choice;
 
 
-    if (choice == 1) {
-        ifstream file(filename);
-        if (file.is_open()) {
-            string line;
-            while (getline(file, line)) {
-                cout << line << endl;
-            }
-            file.close();
+if (choice == 1) {
+    ifstream file(filename);
+    if (file.is_open()) {
+        string line;
+        while (getline(file, line)) {
+            cout << line << endl;
         }
+        file.close();
     }
-    else if (choice == 2) {
-        while (choice == 2) {
-            cout << "1. Âûéòè" << endl << "2. Äîáàâèòü";
-            cin >> choice;
-            if (choice == 2) {
-                ofstream file(filename, ios::app);
-                if (file.is_open()) {
-                    string studentData;
-                    cout << "Ââåäèòå äàííûå ñòóäåíòà: ";
-                    cin.ignore();
-                    getline(cin, studentData);
-                    file << studentData << endl;
-                    file.close();
-                    cout << "Ñòóäåíò äîáàâëåí." << endl;
-                }
-            }
-            else {
-                return 0;
-            }
-        }
-    }
-    else if (choice == 3) {
-        ifstream file(filename, ios::app);
-        if (file.is_open()) {
-            cout << "Ââåäèòå íîìåð ñòðîêè äëÿ óäàëåíèÿ: " << endl;
-            int i_number_line_delete = 0;
-            cin >> i_number_line_delete;
-            int i_number_line_now = 0;
-            string line;
-            string line_file_text;
-            while (getline(file, line)) {
-                i_number_line_now++;
-                if (i_number_line_now != i_number_line_delete) {
-                    line_file_text.insert(line_file_text.size(), line);
-                }
+}
+else if (choice == 2) {
+    while (choice == 2) {
+        cout << "1. Выход" << endl << "2. Добавить" << endl;
+        cin >> choice;
+        if (choice == 2) {
+            ofstream file(filename, ios::app);
+            if (file.is_open()) {
+                string studentData;
+                cout << "Введите данные студента: ";
+                cin.ignore();
+                getline(cin, studentData);
+                file << studentData << endl;
                 file.close();
-                ofstream file_out;
-                file_out.open(filename, ios::trunc | ios::binary);
-                file_out.write(line_file_text.c_str(), line_file_text.size());
-                file_out.clear();
+                cout << "Студент добавлен." << endl;
             }
-            file.close();
-        }
-        return 0;
-    }
-    
-    else if (choice == 4) {
-        ifstream file(filename);
-if (file.is_open()) {
-    string word, vvod;
-    cout << "Введите студента, которого хотите найти: ";
-    cin >> vvod;
-    while (file >> word) {
-        if (word == vvod) {
-            cout << "Результаты поиска: " << word << endl;
-            break;
         }
         else {
-            continue;
+            return 0;
         }
     }
 }
+else if (choice == 3) {
+    ifstream file(filename, ios::app);
+    if (file.is_open()) {
+        cout << "Введите номер строки для удаления: " << endl;
+        int i_number_line_delete = 0;
+        cin >> i_number_line_delete;
+        ofstream outfile("temp.txt");
+
+        if (!outfile.is_open()) {
+            cerr << "Ошибка создания временного файла!" << endl;
+            file.close();
+            return 1;
+        }
+
+        string line;
+        int current_line = 1;
+
+        while (getline(file, line)) {
+            if (current_line != i_number_line_delete) {
+                outfile << line << endl;
+            }
+            current_line++;
+        }
+
+        file.close();
+        outfile.close();
+        if (remove(filename.c_str()) != 0) {
+            cerr << "Ошибка удаления исходного файла!" << endl;
+            return 1;
+        }
+
+        if (rename("temp.txt", filename.c_str()) != 0) {
+            cerr << "Ошибка переименования временного файла!" << endl;
+            return 1;
+        }
+
+        cout << "Строка успешно удалена." << endl;
+
         return 0;
+    }
+}
+
+else if (choice == 4) {
+    ifstream file("file.txt");
+    if (file.is_open()) {
+        string word, vvod;
+        int line = 0;
+        cout << "Введите студента, которого хотите найти: ";
+        cin >> vvod;
+        while (file >> word) {
+            if (word == vvod) {
+                cout << "Result: " << word << endl << "String: " << line;
+                break;
+            }
+            else {
+                continue;
+            }
+        }
+    }
+    return 0;
+}
+else if (choice == 5) {
+    string outputFilename = "output.txt";
+    ifstream file(filename);
+    ofstream outputFile(outputFilename);
+    if (file.is_open()) {
+
+
+        string line;
+
+        while (getline(file, line)) {
+
+            for (auto& c : line) {
+                c = toupper(c);
+            }
+
+            outputFile << line << endl;
+        }
+
+
+        file.close();
+        outputFile.close();
+
+        cout << "Экспорт завершен. Данные записаны в " << outputFilename << endl;
+        return 0;
+    }
+
+}
+else if (choice == 6) {
+    ifstream file(filename);
+    if (file.is_open()) {
+        string data;
+        while (getline(file, data)) { 
+            cout << "Прочитанная строка: " << data << endl;
+        }
+        file.close();
+        return 0;
+    }
+}
+else if (choice == 7) {
+    return 0;
+}
     }
